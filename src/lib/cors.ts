304a86102839077b6f63ae440e43d69f23ea5a87
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2025-08-07 12:54:48 +0200

    update cors settings

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,10 +1,8 @@
 import { env } from "./env";
 
-export const baseOrigins = [
-	"https://squabble.lol",
-	"https://dev.squabble.lol",
-];
+export const baseOrigins = ["https://squabble.lol", "https://dev.squabble.lol"];
 export const localOrigins = [
-	"http://localhost:3000",
-	env.NEXT_PUBLIC_AGENT_URL ?? "http://localhost:3000",
+  "http://localhost:3000",
+  "https://tunnel.*",
+  env.NEXT_PUBLIC_AGENT_URL ?? "http://localhost:3000",
 ];
